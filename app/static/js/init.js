1f4fcd06f9d0f9eda9fc8e6c7c90837a06b9e523
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-05-12 23:19:56 +0200

    update effects and text

---
+++ app/static/js/init.js
@@ -0,0 +1,15 @@
+//import log
+console.log("js init loaded")
+
+
+function toggleView() {
+    // funct log
+    console.log("toggleView called");
+    $("#firstSection").slideUp();
+    $("#secondSection").slideDown();
+}
+
+
+$(function () {
+    // auto loaded js and jquery
+}); 
