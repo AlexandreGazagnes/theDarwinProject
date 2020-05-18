40b958fea1733da865e4c3553dbd8704b26517bc
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-05-19 00:23:10 +0200

    continue fix generic code

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
+import secrets
 from flask import Flask
 from flask import render_template, redirect, escape, request, session
 
 from params import Params
 from src.algo_child import NathanAlgo
-
 
 ALGO = dict()
 
@@ -12,7 +12,7 @@ def make_app():
     """make app """
 
     app = Flask(__name__, template_folder="templates", static_folder="static")
-    app.config["SECRET_KEY"] = "azerty"
+    app.config["SECRET_KEY"] = secrets.token_hex(16)
     from app.front import front
     from app.back import back
 
