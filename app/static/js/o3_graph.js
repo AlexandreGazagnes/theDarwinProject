56e128e2119661312eacc86c11650e30e714f22b
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-05-23 23:22:41 +0200

    we got ok for laptop 14'

--- app/static/js/o3_graph.js
+++ app/static/js/o3_graph.js
@@ -36,6 +36,7 @@ function drawPopChart() {
     // options
     let options = {
         maintainAspectRatio: false,
+        responsive: true,
         animation: {
             duration: 0 // general animation time
         },
@@ -56,8 +57,8 @@ function drawPopChart() {
         }
     });
 
-    scatterChart.canvas.parentNode.style.height = '200px';
-    scatterChart.canvas.parentNode.style.width = '333px';
+    // scatterChart.canvas.parentNode.style.height = '200px';
+    // scatterChart.canvas.parentNode.style.width = '333px';
 }
 
 
@@ -96,53 +97,116 @@ function drawXsChart() {
         options: options
     });
 
-    chart.canvas.parentNode.style.height = '200px';
-    chart.canvas.parentNode.style.width = '333px';
+    //     chart.canvas.parentNode.style.height = '200px';
+    //     chart.canvas.parentNode.style.width = '333px';
 }
 
 function drawYsChart() {
     console.debug("drawYsChart")
-    // update coords
-    if (dynamicState.year != 0) {
-        ysCoords.push(dynamicState.graph.ys_last);
-    }
-    // data
-    let data = google.visualization.arrayToDataTable(ysCoords);
+    // // update coords
+    // if (dynamicState.year != 0) {
+    //     ysCoords.push(dynamicState.graph.ys_last);
+    // }
+    // // data
+    // let data = google.visualization.arrayToDataTable(ysCoords);
+    // // options
+    // let options = {
+    //     // title: "best 'y' value evolution during years",
+    //     hAxis: { title: 'years', }, //  minValue: xMin, maxValue: xMax 
+    //     vAxis: { title: 'y', }, // minValue: yMin, maxValue: yMax 
+    //     legend: 'none',
+    //     width: graphWidth,
+    //     heigh: graphHeight
+    // };
+    // // init chart on DOM element and push
+    // let chart = new google.visualization.LineChart(document.getElementById('ysChart'));
+    // chart.draw(data, options);
+
+    // context
+    var ctx = document.getElementById('ysChart').getContext('2d');
+
+    // labels 
+    let labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    // dataset
+    let datasets = [
+        {
+            label: 'My First dataset',
+            backgroundColor: "#007bff",
+            borderColor: "#007bff",
+            data: [0, 10, 5, 2, 20, 30, 45]
+        },
+    ];
+
     // options
-    let options = {
-        // title: "best 'y' value evolution during years",
-        hAxis: { title: 'years', }, //  minValue: xMin, maxValue: xMax 
-        vAxis: { title: 'y', }, // minValue: yMin, maxValue: yMax 
-        legend: 'none',
-        width: graphWidth,
-        heigh: graphHeight
-    };
-    // init chart on DOM element and push
-    let chart = new google.visualization.LineChart(document.getElementById('ysChart'));
-    chart.draw(data, options);
+    let options = {};
+
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: datasets
+        },
+        options: options
+    });
+
+    // chart.canvas.parentNode.style.height = '200px';
+    // chart.canvas.parentNode.style.width = '333px';
 }
 
 // make a chart 
 function drawYearsChart() {
     console.debug("drawYearssChart")
-    // update coords
-    if (dynamicState.year != 0) {
-        yearsCoords.push(dynamicState.graph.years_last);
-    }
-    // data
-    let data = google.visualization.arrayToDataTable(yearsCoords);
-    // options
-    let options = {
-        // title: "year of best found solution evolution during years",
-        hAxis: { title: 'years', }, // minValue: xMin, maxValue: xMax
-        vAxis: { title: 'year of best solution', }, // minValue: yMin, maxValue: yMax 
-        legend: 'none',
-        width: graphWidth,
-        heigh: graphHeight
-    };
-    // init chart on DOM element and push
-    var chart = new google.visualization.LineChart(document.getElementById('yearsChart'));
-    chart.draw(data, options);
+    // // update coords
+    // if (dynamicState.year != 0) {
+    //     yearsCoords.push(dynamicState.graph.years_last);
+    // }
+    // // data
+    // let data = google.visualization.arrayToDataTable(yearsCoords);
+    // // options
+    // let options = {
+    //     // title: "year of best found solution evolution during years",
+    //     hAxis: { title: 'years', }, // minValue: xMin, maxValue: xMax
+    //     vAxis: { title: 'year of best solution', }, // minValue: yMin, maxValue: yMax 
+    //     legend: 'none',
+    //     width: graphWidth,
+    //     heigh: graphHeight
+    // };
+    // // init chart on DOM element and push
+    // var chart = new google.visualization.LineChart(document.getElementById('yearsChart'));
+    // chart.draw(data, options);
+
+
+    // // context
+    // var ctx = document.getElementById('yearsChart').getContext('2d');
+
+    // // labels 
+    // let labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    // // dataset
+    // let datasets = [
+    //     {
+    //         label: 'My First dataset',
+    //         backgroundColor: "#007bff",
+    //         borderColor: "#007bff",
+    //         data: [0, 10, 5, 2, 20, 30, 45]
+    //     },
+    // ];
+
+    // // options
+    // let options = {};
+
+    // var chart = new Chart(ctx, {
+    //     type: 'line',
+    //     data: {
+    //         labels: labels,
+    //         datasets: datasets
+    //     },
+    //     options: options
+    // });
+
+    // chart.canvas.parentNode.style.height = '200px';
+    // chart.canvas.parentNode.style.width = '333px';
 }
 
 
