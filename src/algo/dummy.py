c455d2f0681993d6274b4dd5ce1106729fa341d6
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-06-01 16:56:57 +0200

    algo

---
+++ src/algo/dummy.py
@@ -0,0 +1,38 @@
+from src import logger
+
+from src.algo.base_algo import _Algo
+from src.functs import *
+
+
+class _DummyAlgo(_Algo):
+    """just a dummy algo for tests"""
+
+    def __init__(self):
+        """init method """
+
+        d = {
+            "_id": "fghjkl",
+            "year": 0,
+            "funct": Functs["x2"],
+            "objective": "min",
+            "interval": [-100, 100],
+            "winning_threshold": 0.005,
+            "seed_parents": 12,
+            "kill_rate": 0.33,
+            "demography": 1,
+            "dimension": "1D",
+            "level": "easy",
+            "average_child_numb": 0.5,
+            "kill_before_reproduce": 1,
+            "social_system": "normal",
+            "current_population": [],
+            "learning_curve": [],
+            "original_population": [],
+            "_round": 7,
+            "is_won": 0,
+            "won_year": -1,
+        }
+
+        logger.warning(str(d))
+
+        super().__init__(**d)
