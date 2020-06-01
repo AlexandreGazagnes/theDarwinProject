f86655f24378772fb84bfc8b81b959ba7db8fffd
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-06-01 16:07:57 +0200

    just rename files

---
+++ src/algo/easy_algo.py
@@ -0,0 +1,35 @@
+from src import logger
+
+from src.algo.algo_base import _Algo
+from src.functs import *
+
+
+class DummyAlgo(_Algo):
+    """just a dummy algo for tests"""
+
+    def __init__(self, _id):
+        """init method """
+
+            _funct = Functs["x2"]
+
+        d = {
+            "_id": _id,
+            "year": 0,
+            "funct": _funct,
+            "objective": _funct["objective"],
+            "interval": _funct["interval"],
+            "winning_threshold": 0.005,
+            "seed_parents": 10,
+            "kill_rate": 0.333,
+            "demography": 1,
+            "average_child_numb": 0.75,
+            "kill_before_reproduce": 0,
+            "social_system": "normal",
+            "current_population": [],
+            "original_population": [],
+            "_round": 7,
+        }
+
+        logger.warning(str(d))
+
+        super().__init__(**d)
