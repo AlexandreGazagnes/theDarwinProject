25c1510e8678f05a755dfaa81cf0c14755a8cbde
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-06-11 16:18:18 +0200

    update

--- infrastructure/sh/boot.prod.sh
+++ infrastructure/sh/boot.prod.sh
@@ -1,9 +1,14 @@
 #! /bin/sh 
 
+
 # vars
+echo "start VARS"
 . ./vars/vars.prod.sh
+echo "end   VARS"
+
 
 # roles
+echo "start ROLES"
 . ./roles/update_upgrade.sh
 . ./roles/install.sh
 . ./roles/localtime.sh
@@ -15,4 +20,4 @@
 . ./roles/ssh_user.sh
 . ./roles/git.sh
 . ./roles/exit.sh
-echo 'done'
+echo "end   ROLES"
