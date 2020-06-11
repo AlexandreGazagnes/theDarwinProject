933e866e4f22a9e9141f6bbc1e8a5bbe8bfb5507
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-06-11 14:40:00 +0200

    update

--- infrastructure/sh/boot.dev.sh
+++ infrastructure/sh/boot.dev.sh
@@ -4,47 +4,60 @@
 . ./vars/vars.dev.sh
 
 # roles
+# echo "start #####################################  $(whoami) --> . ./roles/update_upgrade.sh"
 # . ./roles/update_upgrade.sh
+# echo "end #####################################  $(whoami) --> . ./roles/update_upgrade.sh"
 
+
+echo "start #####################################  $(whoami) --> . ./roles/install.sh"
 . ./roles/install.sh
-echo "#####################################  $(whoami) --> . ./roles/install.sh"
+echo "end #####################################  $(whoami) --> . ./roles/install.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/localtime.sh"
 . ./roles/localtime.sh
-echo "#####################################  $(whoami) --> . ./roles/localtime.sh"
+echo "end #####################################  $(whoami) --> . ./roles/localtime.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/hostname.sh"
 . ./roles/hostname.sh
-echo "#####################################  $(whoami) --> . ./roles/hostname.sh"
+echo "end #####################################  $(whoami) --> . ./roles/hostname.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/create_user.sh"
 . ./roles/create_user.sh
-echo "#####################################  $(whoami) --> . ./roles/create_user.sh"
+echo "end #####################################  $(whoami) --> . ./roles/create_user.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/docker.sh"
 . ./roles/docker.sh
-echo "#####################################  $(whoami) --> . ./roles/docker.sh"
+echo "end #####################################  $(whoami) --> . ./roles/docker.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/ufw.sh"
 . ./roles/ufw.sh
-echo "#####################################  $(whoami) --> . ./roles/ufw.sh"
+echo "end #####################################  $(whoami) --> . ./roles/ufw.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/ssh_config.sh"
 . ./roles/ssh_config.sh
-echo "#####################################  $(whoami) --> . ./roles/ssh_config.sh"
+echo "end #####################################  $(whoami) --> . ./roles/ssh_config.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/ssh_user.sh"
 . ./roles/ssh_user.sh
-echo "#####################################  $(whoami) --> . ./roles/ssh_user.sh"
+echo "end #####################################  $(whoami) --> . ./roles/ssh_user.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/git.sh"
 . ./roles/git.sh
-echo "#####################################  $(whoami) --> . ./roles/git.sh"
+echo "end #####################################  $(whoami) --> . ./roles/git.sh"
 
 
+echo "start #####################################  $(whoami) --> . ./roles/exit.sh"
 . ./roles/exit.sh
-echo "#####################################  $(whoami) --> . ./roles/exit.sh"
+echo "end #####################################  $(whoami) --> . ./roles/exit.sh"
 
 
-echo "#####################################  $(whoami) --> done"
+echo "end #####################################  $(whoami) --> done"
 
