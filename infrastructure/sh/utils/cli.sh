5c72656dcc3953372ed697fc13b12e3f25316be6
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-06-11 22:15:39 +0200

    evil in details

--- infrastructure/sh/utils/cli.sh
+++ infrastructure/sh/utils/cli.sh
@@ -2,3 +2,5 @@ ssh-keygen -f "/home/alex/.ssh/known_hosts" -R "178.79.163.140"
 ssh root@178.79.163.140
 ./run.sh
 ssh -p 24 master@178.79.163.140
+cd theDarwinProject
+docker-compose -p darwin -f theDarwinProject/docker-compose.yml up --build -d 
