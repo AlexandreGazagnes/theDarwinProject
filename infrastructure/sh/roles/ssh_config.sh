fe6cb0465a14b7657df75ccdcbea5d1e30eb1c27
Alexandre GAZAGNES <alexandre@coolparentsmakehappykids.com>
2020-06-12 08:00:31 +0200

    clean code

--- infrastructure/sh/roles/ssh_config.sh
+++ infrastructure/sh/roles/ssh_config.sh
@@ -1,7 +1,7 @@
 echo "\n\nstart #####################################  $(whoami) in $(pwd) --> . ./roles/ssh_config.sh"
 
 # permit root no
-# find /etc/ssh/sshd_config -type f -exec sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' {} \;
+find /etc/ssh/sshd_config -type f -exec sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' {} \;
 # redirect port 
 find /etc/ssh/sshd_config -type f -exec sed -i 's/#Port 22/Port 24/g' {} \;
 # only ssh no login
